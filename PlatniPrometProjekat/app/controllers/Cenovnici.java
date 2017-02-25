88190685b9bbd7cdbc7503a3ae22da5a83d6ed97
Stefan Gabric <stefangabric1@gmail.com>
2017-02-25 17:08:53 +0100

    zapocetiKontroleri

---
+++ PlatniPrometProjekat/app/controllers/Cenovnici.java
@@ -0,0 +1,55 @@
+package controllers;
+
+import java.sql.Date;
+import java.util.List;
+
+import models.Cenovnik;
+import models.Preduzece;
+import play.data.validation.*;
+import play.data.validation.Error;
+import play.mvc.Controller;
+
+public class Cenovnici extends Controller {
+	public static void show(String mode){
+		List<Cenovnik>cenovnici=Cenovnik.findAll();
+		List<Preduzece>preduzeca=Preduzece.findAll();
+		if (mode == null || mode.equals(""))
+			mode = "edit";
+		render(cenovnici,preduzeca, mode);
+	}
+	public static void add(@Required Date datumVazenja, @Required long preduzece) {
+		if(validation.hasErrors()) {
+			for(Error error : validation.errors()) {
+			System.out.println(error.message());
+			}
+			validation.keep();
+			show("add");
+		}else {
+			Cenovnik cenovnik=new Cenovnik();
+			cenovnik.datumVazenja=datumVazenja;
+			cenovnik.preduzece=Preduzece.findById(preduzece);
+			cenovnik.save();
+			validation.keep();
+			show("add");
+	}
+	}
+	public static void filter(Date datum, long preduzece){
+		List<Cenovnik> cenovnici = Cenovnik.find("select * from Cenovnik " +
+			    "where preduzece_id = "+preduzece+" and datumVazenja = "+datum).fetch();
+		String mode = "edit";
+		renderTemplate("Cenovnik/show.html", cenovnici, mode);
+	}
+	public static void edit(Date datumVazenja,long preduzece, long id){
+		Cenovnik cenovnik = Cenovnik.findById(id);
+		cenovnik.datumVazenja = datumVazenja;
+		cenovnik.preduzece = Preduzece.findById(preduzece);
+		cenovnik.save();
+		show("");
+	}	
+	public static void delete(long id){
+		Cenovnik cenovnik = Cenovnik.findById(id);
+		cenovnik.delete();
+		show("");
+	}
+
+}
