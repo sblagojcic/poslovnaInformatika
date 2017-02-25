66066e1c8f8edc2acb4d0451bc17c5353a358382
Stefan Gabric <stefangabric1@gmail.com>
2017-02-25 19:47:59 +0100

    kontroler mesto

---
+++ PlatniPrometProjekat/app/controllers/Mesta.java
@@ -0,0 +1,59 @@
+package controllers;
+
+import java.sql.Date;
+import java.util.List;
+
+import models.Mesto;
+import models.PoslovnaGodina;
+import models.PoslovniPartner;
+import models.Preduzece;
+import play.data.validation.*;
+import play.data.validation.Error;
+import play.mvc.Controller;
+
+public class Mesta extends Controller {
+	public static void show(String mode){
+		List<Mesto>mesta=Mesto.findAll();
+		List<PoslovniPartner>poslovniPartneri=PoslovniPartner.findAll();
+		List<Preduzece>preduzeca=Preduzece.findAll();
+		if (mode == null || mode.equals(""))
+			mode = "edit";
+		render(mesta,preduzeca,poslovniPartneri, mode);
+	}
+	public static void add(@Required String nazivMesta,Long preduzeca,Long poslovniPartner) {
+		if(validation.hasErrors()) {
+			for(Error error : validation.errors()) {
+			System.out.println(error.message());
+			}
+			validation.keep();
+			show("add");
+		}else {
+			Mesto mesto=new Mesto();
+			mesto.poslovniPartneri=PoslovniPartner.findById(poslovniPartner);
+			mesto.preduzeca=Preduzece.findById(preduzeca);
+			mesto.nazivMesta=nazivMesta;
+			mesto.save();
+			validation.keep();
+			show("add");
+	}
+	}
+	public static void filter(@Required String nazivMesta){
+		List<Mesto> mesta = Mesto.find("").fetch();
+		String mode = "edit";
+		renderTemplate("Mesto/show.html", mesta, mode);
+	}
+	public static void edit(@Required String nazivMesta,Long preduzeca,Long poslovniPartner, long id){
+		Mesto mesto = Mesto.findById(id);
+		mesto.poslovniPartneri=PoslovniPartner.findById(poslovniPartner);
+		mesto.preduzeca=Preduzece.findById(preduzeca);
+		mesto.nazivMesta=nazivMesta;
+		mesto.save();
+		show("");
+	}	
+	public static void delete(long id){
+		Mesto mesto = Mesto.findById(id);
+		mesto.delete();
+		show("");
+	}
+
+}
