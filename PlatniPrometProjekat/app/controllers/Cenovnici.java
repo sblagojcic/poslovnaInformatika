de9de9c7153bd27a08378539a993e1b61eb3e602
Stefan Gabric <stefangabric1@gmail.com>
2017-02-28 12:10:26 +0100

    Kopiranje cenovnika sa promenom cene

--- PlatniPrometProjekat/app/controllers/Cenovnici.java
+++ PlatniPrometProjekat/app/controllers/Cenovnici.java
@@ -1,10 +1,12 @@
 package controllers;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import models.Cenovnik;
 import models.Preduzece;
+import models.StavkaCenovnika;
 import play.data.validation.*;
 import play.data.validation.Error;
 import play.mvc.Controller;
@@ -48,6 +50,41 @@ public class Cenovnici extends Controller {
 	public static void delete(long id){
 		Cenovnik cenovnik = Cenovnik.findById(id);
 		cenovnik.delete();
+		show("");
+	}
+	public static void copy(long id,float procenat){
+		Cenovnik cenovnik = Cenovnik.findById(id);
+		Cenovnik cenovnik2=new Cenovnik();
+		cenovnik2.datumVazenja=cenovnik.datumVazenja;
+		cenovnik2.preduzece=cenovnik.preduzece;
+		cenovnik2.stavkeCenovnika=new ArrayList<StavkaCenovnika>();
+		System.out.println(id);
+		System.out.println(procenat);
+		System.out.println(cenovnik2);
+		System.out.println(cenovnik.stavkeCenovnika.size());
+		cenovnik2.save();
+		for (StavkaCenovnika  stavka : cenovnik.stavkeCenovnika) {
+			StavkaCenovnika stavka2=new StavkaCenovnika();
+			stavka2.robaIliUsluga=stavka.robaIliUsluga;
+
+			System.out.println("=========");
+			System.out.println(stavka);
+
+			System.out.println(stavka2);
+			if (procenat>0) {
+				stavka2.cena=stavka.cena+(stavka.cena*procenat/100);
+				
+			}
+			else{
+				procenat=-procenat;
+				stavka2.cena=stavka.cena-(stavka.cena*procenat/100);				
+			}
+			stavka2.cenovnik=cenovnik2;
+			stavka2.save();
+			cenovnik2.stavkeCenovnika.add(stavka2);
+		}
+
+		cenovnik2.save();
 		show("");
 	}
 
