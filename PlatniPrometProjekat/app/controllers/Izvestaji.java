4de790b4de4b88e2f5d019e19b764345e1278e6c
Stevan Blagojcic <sblagojcic@gmail.com>
2017-03-02 21:53:26 +0100

    izvestaji

--- PlatniPrometProjekat/app/controllers/Izvestaji.java
+++ PlatniPrometProjekat/app/controllers/Izvestaji.java
@@ -52,39 +52,20 @@ public class Izvestaji extends Controller {
 			}
 			show("");
 	}	
-	public static void report2(@Required Long id){
-
+	public static void report2(@Required long id){
+		try {
+	        String status = "P";  // i sve vrste
+			Map params = new HashMap(1);
+			params.put("id", id);
+			JasperPrint jp = JasperFillManager.fillReport( "C://Users/Korisnik/workspacePoslovna/poslovnaInformatika/PlatniPrometProjekat/lib/faktura.jasper",params,DriverManager.getConnection("jdbc:mysql://localhost:3306/projekatplay", "root", "1234") );
+			JasperViewer.viewReport(jp, false);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
 			show("");
 	}	
 	
-	 static Connection getSimpleConnection() {
-		    //See your driver documentation for the proper format of this string :
-		    String DB_CONN_STRING = "jdbc:mysql://localhost:3306/projekatplay";
-		    //Provided by your driver documentation. In this case, a MySql driver is used : 
-		    String DRIVER_CLASS_NAME = "org.gjt.mm.mysql.Driver";
-		    String USER_NAME = "root";
-		    String PASSWORD = "1234";
-		    
-		    Connection result = null;
-		    try {
-		      Class.forName(DRIVER_CLASS_NAME).newInstance();
-		    }
-		    catch (Exception ex){
-		      log("Check classpath. Cannot load db driver: " + DRIVER_CLASS_NAME);
-		    }
-
-		    try {
-		      result = (Connection) DriverManager.getConnection(DB_CONN_STRING, USER_NAME, PASSWORD);
-		    }
-		    catch (SQLException e){
-		      log( "Driver loaded, but cannot connect to db: " + DB_CONN_STRING);
-		    }
-		    return result;
-		  }
-
-		  private static void log(Object aObject){
-		    System.out.println(aObject);
-		  }
+	 
 		
 
 	
